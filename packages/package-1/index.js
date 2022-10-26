d13ccd825e177bcdc28d7139c4e188ec0f109370
Nguyen Phat <phatnt.uit@gmail.com>
2022-10-26 23:54:00 +0700

    feat: upgrade to lerna@6

--- packages/package-1/index.js
+++ packages/package-1/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+const dayjs = require('dayjs');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+
+dayjs.extend(customParseFormat);
+
+const DEFAULT_DATE_FORMAT = 'DD/MM/YYYY';
+
+/**
+ * Get current age
+ * @param {string} dob date of birth in format DD/MM/YYYY
+ * @returns {number} currentAge
+ */
+const getCurrentAge = dob => {
+  const today = dayjs();
+  const dobObj = dayjs(dob, DEFAULT_DATE_FORMAT);
+
+  return today.diff(dobObj, 'year');
+};
+
+module.exports = {
+  getCurrentAge,
+};
---
+++ packages/package-2/index.js
@@ -0,0 +1,3 @@
+const { getCurrentAge } = require('package-1');
+
+console.log(getCurrentAge('28/06/1995'));
